abf410589fa055977ebc359f341007d2a93640ca
matheusBBarni <brehm.matheus@hotmail.com>
2018-11-06 21:39:22 -0200

    wip

--- Sistema_Biblioteca/Includes/header.php
+++
@@ -1,6 +0,0 @@
-<meta charset="UTF-8">
-<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.js"></script>
-<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js"></script>
-<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" />
-<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.5.0/css/all.css" integrity="sha384-B4dIYHKNBt8Bc12p+WXckhzcICo0wtJAoU8YZTY5qE0Id1GSseTk6S+L3BlXeVIU" crossorigin="anonymous">
-<!--Para pegar icones https://fontawesome.com/icons?d=gallery&m=free -->
