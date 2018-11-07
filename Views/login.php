e2bdcfe464efd248a7aef7a1c1da2ca24da0d0fc
matheusBBarni <brehm.matheus@hotmail.com>
2018-11-07 21:48:40 -0200

    PG login arrumada

--- Views/login.php
+++ Views/login.php
@@ -16,17 +16,15 @@
                             <input type="email" id="inputEmail" name="inputEmail" class="form-control" placeholder="Email / Usuário" required autofocus>
                         </div>
                     </div>
-                    <div class="row row-form senha-login">
+                    <div style="margin-bottom: 10px" class="row row-form senha-login">
                         <div class="txt-login col-sm-12 col-md-12">
-                            <input type="password" id="inputSenha" name="inputSenha" class="form-control" placeholder="Senha" required>
-                            <a href="esqueci.php"><small id="esqueci" class="form-text text-muted">Esqueci minha senha</small></a>
+                            <input maxlength="16" type="password" id="inputSenha" name="inputSenha" class="form-control" placeholder="Senha" required>
+                            <a href="esqueci.php"><small id="esqueci" class="form-text text-muted"></small></a>
                         </div>
                     </div>
                     <div class="row row-form">
                         <div class="txt-login col-sm-12 col-md-12">
                             <button class="btn btn-lg btn-primary btn-block" type="submit">Entrar</button>
-                            <h1 class="h6 mb-3 font-weight-normal">Primeiro acesso? Registre-se!</h1>
-                            <button class="btn btn-lg btn-secondary btn-block" id="btnRegistrar" type="button">Registrar</button>
                         </div>
                     </div> 
                 </form>
