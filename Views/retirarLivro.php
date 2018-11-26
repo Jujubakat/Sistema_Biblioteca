420e471c799aca4bdc62b889e06123d94eaea506
FelKhar <a.fayterna@hotmail.com>
2018-11-26 21:41:03 -0200

    Continuação [Alugar]

--- Views/retirarLivro.php
+++ Views/retirarLivro.php
@@ -12,23 +12,15 @@
 						<div class="form-group col-xs-6 col-lg-6">
 							<label for="inputEmail"><i class="fas fa-envelope"></i>  Email</label>
 							<input type="email" class="form-control" id="inputEmail" name="inputEmail" aria-describedby="emailHelp" placeholder="Informe o email">
-						</div>
-						<div class="form-group col-xs-6 col-lg-6">
-							<label for="inputNome"><i class="fas fa-user-alt"></i>  Nome</label>
-							<input type="text" class="form-control" id="inputNome" name="inputNome" aria-describedby="nomeHelp" placeholder="Informe o nome completo">
-						</div>
+						</div>						
 					</div>
 					<div class="row">
 						<div class="form-group col-xs-4 col-lg-4">
 							<label for="inputSenha"><i class="fas fa-unlock"></i>  Senha</label>
-							<input type="password" class="form-control" id="inputSenha" name="inputSenha" placeholder="Informe uma senha">
-							</div>
+							<input type="password" class="form-control" id="inputSenha" name="inputSenha" placeholder="Informe sua senha">
+							</div>						
 						<div class="form-group col-xs-4 col-lg-4">
-							<label for="inputCpf"><i class="fas fa-id-card"></i>  CPF</label>
-							<input type="text" class="form-control" id="inputCpf" name="inputCpf" aria-describedby="CpfHelp" placeholder="Informe o CPF">
-						</div>
-						<div class="form-group col-xs-4 col-lg-4">
-							<label for="inputCpf"><i class="fas fa-braille"></i>  Tipo do usuário</label>
+							<label for="inputLivro"><i class="fas fa-braille"></i>  Livro</label>
 							<select class="form-control" id="selectTipo" name="selectTipo" aria-describedby="tipoHelp">
 								<option value="A">Aluno</option>
 								<option value="P">Professor</option>
