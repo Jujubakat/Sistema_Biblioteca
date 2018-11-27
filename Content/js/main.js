be8bceab100530dc55b979cb370424c8ebd68ab1
matheusBBarni <brehm.matheus@hotmail.com>
2018-11-26 22:39:12 -0200

    wip

--- Content/js/main.js
+++ Content/js/main.js
@@ -1,4 +1,34 @@
 /*MAIN JS*/
 jQuery(document).ready(function($) {
-    $
+    /*$scope.atualizarTabelaPrincipal = function () {
+		$http.get($scope.WebService).then(function (response) {
+			if ( response.data ) {
+				$('#tabela-principal').bootstrapTable('destroy');
+				$('#tabela-principal').bootstrapTable({data: response.data});
+			} else {
+				$('#tabela-principal').bootstrapTable('destroy');
+				$('#tabela-principal').bootstrapTable({data: []});
+			}
+		});
+    }*/
+    var urlGet = 'http://localhost/api_sistema-biblioteca/livro/read.php';
+    $.get( 'http://localhost/api_sistema-biblioteca/livro/read.php', function(data) {
+        $('#tabela-livros').bootstrapTable('destroy');
+		$('#tabela-livros').bootstrapTable({data: data});
+    });
+    /*
+    $.ajax({
+        url:'http://localhost/api_sistema-biblioteca/livro/read.php',
+        type:'GET',
+        headers: { 
+            'Accept': 'application/json',
+            'Content-Type': 'application/json' 
+        },
+        dataType: 'json',
+        data: JSON.stringify(dados),
+        success: function(data) {
+            $('#tabela-livros').bootstrapTable('destroy');
+			$('#tabela-livros').bootstrapTable({data: data});
+        }
+    });*/
 });
--- Views/listagem.php
+++ Views/listagem.php
@@ -6,8 +6,8 @@
 <body>
 		<div class="container-fluid">
 		<h1><i class="fas fa-list-alt"></i>  Livros</h1>
-		<div>
-			<table id="tabela-livros" data-url="http://localhost/api_sistema-biblioteca/livro/read.php" data-toggle="table" data-url="WS/Usuario"
+		<!--<div>
+			<table id="tabela-livros" data-toggle="table" data-url="WS/Usuario"
 				data-striped="true" data-sortable="true"
 				data-classes="table table-hover table-striped table-condensed" data-refresh="true">
 				<thead>
@@ -21,8 +21,8 @@
 					</tr>
 				</thead>
 			</table>
-		</div>
-		<!--<div>
+		</div>-->
+		<div>
 			<?php
 			require_once("model/Connection.php");
 
@@ -58,7 +58,7 @@
 						echo "0 results"; 	
 						}			
 				?>
-			</div>-->
+			</div>
 		</div>
 </body>
 </html>
