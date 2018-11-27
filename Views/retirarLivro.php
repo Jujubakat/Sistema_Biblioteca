3a7dd91c05b50364eae8a418d736c98739f4d5e9
FelKhar <a.fayterna@hotmail.com>
2018-11-27 21:22:20 -0200

    alteração banco de dados + code [alugar]

--- Views/retirarLivro.php
+++ Views/retirarLivro.php
@@ -7,6 +7,24 @@
 			<h1><i class="fab fa-algolia"></i> Alugar</h1>
 			<div id="divMensagem"></div>
 			<div class="container">
+
+				<?php
+					require_once("model/Connection.php");
+
+					$connection = new Connection();
+        			$conn = $connection->getConn();
+
+        			if ($conn->connect_error) {
+				    die("Erro de conexão: " . $conn->connect_error);
+				} 
+
+
+					$sql = "SELECT nome_livro FROM livro
+							WHERE status_livro = 'D'";
+					$result = $conn->query($sql);
+
+				?>
+
 				<form id="formCadastroUsuario" class="form-cadastrar">
 					<div class="row">
 						<div class="form-group col-xs-6 col-lg-6">
@@ -22,9 +40,9 @@
 						<div class="form-group col-xs-4 col-lg-4">
 							<label for="inputLivro"><i class="fas fa-braille"></i>  Livro</label>
 							<select class="form-control" id="selectTipo" name="selectTipo" aria-describedby="tipoHelp">
-								<option value="A">Aluno</option>
-								<option value="P">Professor</option>
-								<option value="C">Comunidade</option>
+								<?php while($row = mysql_fetch_array($result)):;?>
+								<option value="<?php echo $row[0];?>"><?php echo $row[1];?></option>
+							<?php endwhile;?>
 							</select>
 						</div>
 					
